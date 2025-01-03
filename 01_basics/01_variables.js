059ca66af9702fa120b4255240c668215477f8b0
Versha Dubey <110320262+VershaDubey@users.noreply.github.com>
2025-01-03 13:56:11 +0000

    commit it

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144687
+let accountEmail = "versha@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+accountEmail = "hp.com"
+accountPassword = "4567"
+accountCity = "ballia"
+let accountState;
+// accountId = 3
+console.log(accountPassword);
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+// treate all js code as newer version mode
+// alert("hello") we are using node js not broswer
+console.log(3 + 3)
+console.log("versha");
+let name = "versha"
+let age = 19
+let isLoogedIn = false
+let state;
+
+
+//number => 2  to the power of 53
+//bigint
+//string => ""
+// boolean => true/false
+// null standalon value
+// undefined => 
+    // symbol => unique 
+
+
+//object 
+console.log(typeof undefined) //undefined eska type hai
+console.log(typeof null) // object hai
