5ac3f27eb14810e6274c7724f79d37785758b297
Versha Dubey <110320262+VershaDubey@users.noreply.github.com>
2025-01-04 09:11:14 +0000

    conversion of data types

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+let score = "mini"
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+//"88" => 88
+//"34ffg" => nan
+// true =>1,false => 0
+let isLoggedIn = "mini"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+// 1 => true , 0 => false
+// "" => false
+// "versha" => true
+let someNumber = 66
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
